a73fc1b3aee93c83a71a9590b97e5f29b2494cca
ZhenyanZhang <59685284+ZhenyanZhang@users.noreply.github.com>
2021-12-10 14:57:38 +0800

    Codes and Scripts for Figure 1-4

---
+++ Figure 1A_Map.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(ggmap)
+#library(sp)
+#library(maptools)
+#library(maps)
+#library(ggpubr)
+
+data <- read.delim('map.txt', row.names = 1, sep = '\t', stringsAsFactors = FALSE, check.names = FALSE,na.strings="na")
+#datas =read.table("suppressive map.txt",header = T, row.names = 1)
+mp<-NULL #null map
+mapworld<-borders("world",colour = NA,fill="gray") #basic map
+mp<-ggplot()+mapworld+ylim(-90,90)+theme(panel.grid = element_blank(), panel.background = element_rect(color = 'black', fill = 'transparent'), legend.key = element_rect(fill = 'transparent'))
+mp2<-mp+geom_point(aes(x=data$Longitude,y=data$Latitude,size=data$Sample_number,color=data$Habitat),alpha=0.8)+scale_size(range=c(1,15))
+mp2
