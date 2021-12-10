42529d1046b3487a9bffaf28077948b37ad15d49
ZhenyanZhang <59685284+ZhenyanZhang@users.noreply.github.com>
2021-12-10 17:23:27 +0800

    Delete F-statistic.R

--- F-statistic.R
+++
@@ -1,5 +0,0 @@
-# create a dataset
-data <- read.delim('MGE-ARG.txt', row.names = 1, sep = '\t', stringsAsFactors = FALSE, check.names = FALSE,na.strings="na")
-
-model<-lm(MGE~ARG,data=data)
-summary(model)
